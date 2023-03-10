c68e3842431c8f0e63a0e34a167f0202681e1037
boissy.jeremy@yahoo.fr <boissy.jeremy@yahoo.fr>
2023-03-10 17:35:40 +0100

    Adding the polymorphic contener

---
+++ Sparkle/includes/Structure/DataContener/spk_polymorphic_conterner.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <vector>
+
+namespace spk
+{
+	class PolymorphicContainer
+	{
+	private:
+		unsigned int _id;
+		size_t _size;
+		size_t _readed;
+
+		std::vector<uint8_t> _content;
+
+	public:
+		PolymorphicContainer()
+		{
+			clear();
+		}
+
+		bool empty() const
+		{
+			return (_readed >= _size);
+		}
+
+		size_t getSize() const
+		{
+			return _size;
+		}
+
+		size_t getReaded() const
+		{
+			return _readed;
+		}
+
+		void clear()
+		{
+			_id = 0;
+			_size = 0;
+			_readed = 0;
+			_content.clear();
+		}
+
+		template<typename DataType>
+		PolymorphicContainer& operator << (const DataType& data)
+		{
+			static_assert(std::is_standard_layout<DataType>::value, "Data is too complex to be pushed into vector");
+
+			size_t old_size = _content.size();
+			size_t data_size = sizeof(DataType);
+
+			if (_content.max_size() - _content.size() < data_size) {
+				throw std::length_error("PolymorphicContainer: adding this data will exceed maximum size of the vector.");
+			}
+
+			content.resize(_content.size() + sizeof(DataType));
+
+			std::memcpy(_content.data() + old_size, &data, sizeof(DataType));
+
+			_size = _content.size();
+
+			return (*this);
+		}
+
+		template<typename DataType>
+		PolymorphicContainer& operator >> (DataType& data)
+		{
+			static_assert(std::is_standard_layout<DataType>::value, "Data is too complex to be pulled from vector");
+
+			size_t next_size = _readed;
+
+			std::memcpy(&data, _content.data() + next_size, sizeof(DataType));
+
+			_readed += sizeof(DataType);
+
+			return (*this);
+		}
+	};
+}
