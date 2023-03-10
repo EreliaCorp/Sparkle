438812f730740dfcc700eb721a502e62e0ff7935
boissy.jeremy@yahoo.fr <boissy.jeremy@yahoo.fr>
2023-03-10 18:34:02 +0100

    Adding static attributes for spk::color

--- Sparkle/src/sparkle.cpp
+++ Sparkle/src/sparkle.cpp
@@ -4,4 +4,11 @@ namespace spk
 {
 	thread_local spk::IOStream cout = spk::IOStream(std::cout);
 	thread_local spk::IOStream cerr = spk::IOStream(std::cerr);
+
+    const Color Color::transparent = Color(0.0f, 0.0f, 0.0f, 0.0f);
+    const Color Color::white = Color(1.0f, 1.0f, 1.0f, 1.0f);
+    const Color Color::black = Color(0.0f, 0.0f, 0.0f, 1.0f);
+    const Color Color::red = Color(1.0f, 0.0f, 0.0f, 1.0f);
+    const Color Color::green = Color(0.0f, 1.0f, 0.0f, 1.0f);
+    const Color Color::blue = Color(0.0f, 0.0f, 1.0f, 1.0f);
 }
